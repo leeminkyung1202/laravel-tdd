7198c32a528343ef3b5ff6f97d2d4c75fc4c1852
이민경 <dev13@applmail.com>
2019-07-31 18:58:38 +0900

    laravel 1차 tdd 완료

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,16 +3,8 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Foundation\Testing\DatabaseMigrations as DatabaseMigrations;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, DatabaseMigrations;
-
-    public function setUp() : void
-    {
-        parent::setUp();
-
-        $this->runDatabaseMigrations();
-    }
+    use CreatesApplication;
 }
