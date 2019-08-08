2676fd4d480c79272df6d29ed49ef9b6eacbed1b
이민경 <dev13@applmail.com>
2019-08-08 18:59:22 +0900

    스타일 정리

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,15 +1,7 @@
-<!doctype html>
+@extends ('layouts.app')
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
-</head>
-<body>
-    <form method="POST" action="/projects" class="container" style="padding-top: 40px;">
+@section('content')
+    <form method="POST" action="/projects">
         @csrf
         <h1 class="heading is-1">작성</h1>
 
@@ -32,8 +24,8 @@
         <div class="fieId">
             <div class="control">
                 <button type="submit" class="button is-link">생성</button>
+                <a href="/projects">취소</a>
             </div>
         </div>
     </form>
-</body>
-</html>
+@endsection
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,14 +1,10 @@
-<!doctype html>
+@extends ('layouts.app')
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>board</h1>
+@section('content')
+    <div class="flex items-center mb-3">
+        <h1 class="mr-auto">리스트</h1>
+        <a href="/projects/create">작성하기</a>
+    </div>
 
     <ul>
         @forelse ($projects as $project)
@@ -19,5 +15,4 @@
             <li>데이터 없음.</li>
         @endforelse
     </ul>
-</body>
-</html>
+@endsection
